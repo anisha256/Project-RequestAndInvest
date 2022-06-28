89fdbd42e3565c0c427684343ce0dbfbc6474876
anisha256 <1aanisha.rai@gmail.com>
2022-06-29 03:08:25 +0545

    fetch emails of applicants

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,4 @@
+// const nodemailer = require('nodemailer');
 const crypto = require('crypto');
 const User = require('../models/User');
 const Project = require('../models/Project');
@@ -105,11 +106,33 @@ const deactivateUser = async (req, res) => {
 
 const sendMail = async (req, res, next) => {
   try {
-    const project = await Project.find({ isGranted: true });
-    const grantedEmails = [];
-
-    const emails = project.email;
+    const projects = await Project.find({ isGranted: true });
+    const emails = [];
+    projects.forEach((project) => {
+      emails.push(project.email);
+    });
     console.log(emails);
+    // respomsible for sending mail
+    // const mailTransporter = nodemailer.createTransport({
+    //   service: 'gmail',
+    //   auth: {
+    //     user: '1aanisha.rai@gmail.com',
+    //     pass: '',
+    //   },
+    // });
+    // const details = {
+    //   from: '1aanisha.rai@gmail.com',
+    //   to: 'appyrai6@gmail.com',
+    //   subject: 'Request Granted',
+    //   text: 'Congratulations.Your request for the project has been granted.',
+    // };
+    // mailTransporter.sendMail(details, (err) => {
+    //   if (err) {
+    //     console.log('it has an error', err);
+    //   } else {
+    //     console.log('email has sent');
+    //   }
+    // });
     res.status(200).json({
       status: true,
       statusCode: 200,
