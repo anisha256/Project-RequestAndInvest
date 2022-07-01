5e1d6c64bd3018eb8951827207288bea32e9ea3d
anisha256 <1aanisha.rai@gmail.com>
2022-07-02 04:34:17 +0545

    upload route

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -12,3 +12,32 @@ const storage = multer.diskStorage({
     callback(null, `${file.fieldname}-${path.extname(file.originalname)}`);
   },
 });
+
+// function checkFileType(file, callback) {
+//   const filename = /jpg|jpeg|png/;
+//   const extname = filetypes.test(path.extname(file.originalname.toLowerCase()));
+//   const mimetype = filetypes.test(file.mimetype);
+//   if (extname && mimetype) {
+//     return callback(null, true);
+//   } else {
+//     callback('image only');
+//   }
+// }
+// const upload = multer({
+//   storage,
+//   fileFilter: function (req, file, callback) {
+//     checkFileType(file, callback);
+//   },
+// });
+const upload = multer({
+  storage,
+});
+
+router.post(
+  '/',
+  upload.single('image', (req, res) => {
+    res.send(`/${req.file.path}`);
+  })
+);
+
+module.exports = router;
